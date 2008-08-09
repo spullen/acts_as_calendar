18bb65fab1da60ec824ded81043e79f4db4a8b73
Scott Pullen <spullen@spullen-laptop.(none)>
2008-08-09 12:33:07 -0400

    added more functionality to acts_as_calendar module, overlap checking between current event and existing events

--- lib/acts_as_calendar.rb
+++ lib/acts_as_calendar.rb
@@ -7,6 +7,12 @@ module ActsAsCalendar
 end
 
 module ClassMethods
+  ########################################
+  #
+  # acts_as_calendar gives your model the calendar functionality
+  # @params options Hash, optional
+  #
+  ########################################
   def acts_as_calendar(options={})
     cattr_accessor :calendar_start_dt_field, :calendar_end_dt_field, :calendar_event_title_field
     
@@ -17,7 +23,17 @@ module ClassMethods
     send :include, InstanceMethods
   end
   
-  # generate the calendar for month
+  ########################################
+  #
+  # generate a calendar
+  # call this function like YourModel.calendar(...)
+  # @params year Integer
+  # @params month Integer
+  # @params day Integer, optional
+  # @params optional_params Hash, optional
+  # @returns events, cal Array(Hash, Calendar)
+  # 
+  ########################################
   def calendar(year, month, day=nil, optional_params = {})
     attr_accessor :events # a hash to put events into that will be parsed
     attr_accessor :cal # the calendar object, stores useful information about the date range
@@ -80,14 +96,122 @@ module ClassMethods
 end
 
 module InstanceMethods
-  # add any instance methods for the object
-  # ex: @user.some_instance_method
   
-  # TODO: methods to add:
+  ####################################
   #
-  # overlaps with existing event
-  # conflicts with existing event
-  # conflicts with existing event with conditions on it (like only reservations in one location)
-  # 
+  # Checks to see if an event being created overlaps with any
+  # other event already created
+  # @params extra_conditions Array, optional this is a conditional array [string, val_1, ... , val_n]
+  # @returns Boolean false if there are no overlaps, otherwise true
   #
+  ####################################
+  def overlaps_with_existing?(extra_conditions = nil)
+    # get the start and end date of the object
+    start_date = Date.parse(self.read_attribute(self.class.calendar_start_dt_field).to_s)
+    end_date = Date.parse(self.read_attribute(self.class.clendar_end_dt_field).to_s)
+    
+    conditions = Array.new
+    vals = Array.new
+    
+    # add the default conditions and vals
+    conditions << self.class.calendar_start_dt_field + ' >= ?'
+    vals << start_date
+    conditions << self.class.calendar_end_dt_field + ' <= ?'
+    vals << end_date
+    
+    conditions = conditions.join(' AND ')
+    
+    # check to see if there are any extra_conditions and add them
+    unless extra_conditions.nil?
+      conditions += extra_conditions.shift.to_s
+      extra_conditions.each do |val|
+        vals << val
+      end
+    end
+    
+    conditions = [conditions] + vals
+    
+    # get all of the calendar events in that range
+    events = find(:all, 
+                              :conditions => conditions,
+                              :order => self.class.calendar_start_dt_field)
+   
+    # iterate over the events found and check if there are any overlaps
+    overlaps = false
+    current_start_dt = self.read_attribute(self.class.calendar_start_dt_field) 
+    current_end_dt = self.read_attribute(self.class.calendar_end_dt_field)
+    
+    events.each do |event|
+      tmp_start_dt = event.read_attribute(event.class.calendar_start_dt_field)
+      tmp_end_dt = event.read_attribute(event.class.calendar_end_dt_field)
+      
+      if current_start_dt.between?(tmp_start_dt, tmp_end_dt) || current_end_dt.between?(tmp_start_dt, tmp_end_dt)
+        overlaps = true
+      end
+        
+      # if there is an overlap then stop the looping
+      break if overlaps
+    end
+    
+    return overlaps
+  end
+  
+  #######################################
+  #
+  # Checks to see if there are any overlapping events
+  # Similar to overlaps_with_existing? except this will return a 
+  # recordset of the events that overlap with the current one
+  # @params Array, optional: pass in a conditional array to constrain your search more
+  # @returns Array || nil if there aren't any
+  #
+  #######################################
+  def overlaps_with_existing(extra_conditions=nil)
+    # get the start and end date of the object
+    start_date = Date.parse(self.read_attribute(self.class.calendar_start_dt_field).to_s)
+    end_date = Date.parse(self.read_attribute(self.class.clendar_end_dt_field).to_s)
+    
+    conditions = Array.new
+    vals = Array.new
+    
+    # add the default conditions and vals
+    conditions << self.class.calendar_start_dt_field + ' >= ?'
+    vals << start_date
+    conditions << self.class.calendar_end_dt_field + ' <= ?'
+    vals << end_date
+    
+    conditions = conditions.join(' AND ')
+    
+    # check to see if there are any extra_conditions and add them
+    unless extra_conditions.nil?
+      conditions += extra_conditions.shift.to_s
+      extra_conditions.each do |val|
+        vals << val
+      end
+    end
+    
+    conditions = [conditions] + vals
+    
+    # get all of the calendar events in that range
+    events = find(:all, 
+                              :conditions => conditions,
+                              :order => self.class.calendar_start_dt_field)
+   
+    # iterate over the events found and check if there are any overlaps
+    overlaps = Array.new
+    current_start_dt = self.read_attribute(self.class.calendar_start_dt_field) 
+    current_end_dt = self.read_attribute(self.class.calendar_end_dt_field)
+    
+    events.each do |event|
+      tmp_start_dt = event.read_attribute(event.class.calendar_start_dt_field)
+      tmp_end_dt = event.read_attribute(event.class.calendar_end_dt_field)
+      
+      if current_start_dt.between?(tmp_start_dt, tmp_end_dt) || current_end_dt.between?(tmp_start_dt, tmp_end_dt)
+        overlaps << event
+      end
+    end
+    
+    overlaps = nil unless overlaps.size > 0
+    
+    return overlaps
+  end
 end
